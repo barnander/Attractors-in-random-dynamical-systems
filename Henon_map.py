8f6c114a959259cec2d428a04a0fb4cca3afa58a
barnander <nk21253@bristol.ac.uk>
2024-10-07 18:51:38 +0100

    MAde scatter plot to visually check that the random noise is sampled from a uniform distribution over the disk of radius e

--- Henon_map.py
+++ Henon_map.py
@@ -6,9 +6,9 @@ import matplotlib.animation as animation
 
 
 #%% Set up henon map with random bounded noise
-def Henon(x,a = 1.4,b = 0.3, e = 0.6):
+def Henon(x,a = 0.607,b = 0.3, e = 0.6):
     """
-    Computes the Henon map with bounded noise.
+    Computes the Henon map with additive, spherical bounded noise.
     Parameters:
         x (array): 2D array of the current state of the system.
         a (float): Parameter of the Henon map.
@@ -53,42 +53,24 @@ def plotHenon(x0, n, a = 0.06, b = 0.3, b_val = 1, sig = 1, mu = 0):
     plt.title('Henon map with bounded noise')
     plt.show()
 
-def animHenon(x0, n, a = 0.06, b = 0.3, b_val = 1, sig = 1, mu = 0):
-    """
-    Animates the Henon map with bounded noise.
-    Parameters:
-        x0 (array): 2D array of the initial state of the system.
-        n (int): Number of iterations.
-        a (float): Parameter of the Henon map.
-        b (float): Parameter of the Henon map.
-        b_val (float): Bound of the noise.
-        sig (float): Standard deviation of the noise.
-        mu (float): Mean of the noise.
-    """
-    fig, ax = plt.subplots()
-    x = x0
-    x_vals = np.zeros((2,n))
-    for i in range(n):
-        x_vals[:,i] = x
-        x = Henon(x,a,b,b_val,sig,mu)
-    x_vals = x_vals.T
-    x_vals = np.array(x_vals)
-    x_vals = x_vals.reshape(n,2)
-    x_vals = x_vals.T
-    line, = ax.plot(x_vals[0,:],x_vals[1,:],'o')
-    def animate(i):
-        line.set_data(x_vals[0,:i],x_vals[1,:i])
-        return line,
-    ani = animation.FuncAnimation(fig, animate, frames=n, interval=50, blit=True)
-    plt.xlabel('x1')
-    plt.ylabel('x2')
-    plt.title('Henon map with bounded noise')
-    plt.show()
 
 #%%
-x0 = np.array([0.1,0.1])
-n = 100
-plotHenon(x0,n,sig = 0.005)
+#checking random noise distribution
+e = 0.6
+n = 1000
+angle = random.rand(1,n)*2*np.pi #generate random angle in radians
+    #to ensure uniform distribution over disk, radius is square root 
+    #of random number uniformly distributed in [0,1] scaled by e
+radius = np.sqrt(random.rand(1,n))*e 
+bounded_noise = np.vstack((radius*np.cos(angle),radius*np.sin(angle)))
+    
+#plot circle radius e
+theta = np.linspace(0,2*np.pi,100)
+x = e*np.cos(theta)
+y = e*np.sin(theta)
+plt.plot(x,y)
+#plot bounded noise
+plt.scatter(bounded_noise[0,:],bounded_noise[1,:])
+plt.show()
 # %%
-animHenon(x0,n,sig = 0.005)
 # %%
